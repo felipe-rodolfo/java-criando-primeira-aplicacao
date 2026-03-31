d0b331ce7f44300f7e371baa01e103dbefefee29
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2026-03-31 17:02:29 -0300

    feat: atribuindo valores ao filme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     static void main() {
-        System.out.println("Alô Mundo!");
+
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = false;
     }
 }
