35a3be59adfdadeef31e5da4d43ecd0ab4d38951
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2026-04-01 10:25:58 -0300

    challange

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
     static void main() {
 
-        System.out.println("Esse é o Screen Match!");
-        System.out.println("Filme: Top Gun: Maverick");
+        String nome = "Felipe";
+        String tipoConta = "corrente";
+        double saldo = 2500;
+        System.out.println("***********************");
+        System.out.println("Dados iniciais do cliente");
+        System.out.println();
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo inicial: " + saldo);
+        System.out.println();
+        System.out.println("Operações");
+        int operacao = 0;
+        while(operacao != 4) {
+            System.out.println("1- Consultar saldos");
+            System.out.println("2- Receber valor");
+            System.out.println("3- Transferir valor");
+            System.out.println("4- Sair");
+            System.out.println();
+            System.out.println("Digite a opção desejada");
+            Scanner sc = new Scanner(System.in);
+            int opcao = sc.nextInt();
 
-        int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = false;
-        double notaDoFilme = 8.1;
-
-        double media = (9.8 + 6.3 + 8.0) / 3;
-        System.out.println(media);
-        String sinopose;
-        sinopose = """
-                Filme de aventura com galã dos anos 80
-                Muito bom!
-                Ano de lançamento
-                """ + anoDeLancamento;
-        System.out.println(sinopose);
-
-        int classificacao;
-        classificacao = (int) (media / 2);
-        System.out.println(classificacao);
+            switch (opcao) {
+                case 1:
+                    System.out.println();
+                    System.out.println("O saldo atual é R$ " + saldo);
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println("Até a próxima");
+                    operacao = 4;
+                    break;
+                case 2:
+                    System.out.println();
+                    System.out.println("Informe o valor a receber");
+                    double valorReceber = sc.nextDouble();
+                    saldo += valorReceber;
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println();
+                    System.out.println("Informe o valor a transferir");
+                    double valorTransferir = sc.nextDouble();
+                    if (valorTransferir > saldo) {
+                        throw new IllegalArgumentException("Saldo insuficiente");
+                    }
+                    saldo -= valorTransferir;
+                    System.out.println("Saldo atualizado R$ " + saldo);
+                    System.out.println();
+                    break;
+            }
+        }
     }
 }
